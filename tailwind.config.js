0c1e429435bc4408f364a16334d474c8be458fac
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-06-09 18:09:30 +0200

    project part 3 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./build/*.html'],
-  theme: {
-    extend: {},
+  theme: 
+  {
+    extend: 
+    {
+      screens:
+      {
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'}, 
+        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'}
+      }
+    },
   },
   plugins: [],
 }
