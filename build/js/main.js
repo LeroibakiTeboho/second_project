c54964228d6f710b8d000d20a9aa8f036e5dce0a
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-06-10 15:14:11 +0200

    project inprogress

---
+++ build/js/main.js
@@ -0,0 +1,37 @@
+const initApp = () => {
+
+    // get ids
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    // toggle function
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+    }
+
+    hamburgerBtn.addEventListener( 'click', toggleMenu);
+    mobileMenu.addEventListener( 'click', toggleMenu);
+}
+
+// This code means that the 'initApp' function will be called once the HTML document has been fully loaded and parsed. It ensures that initialization code runs only after the document is ready.
+document.addEventListener('DOMContentLoaded', initApp);
+
+
+
+
+// const initApp = () => {
+//     const hamburgerBtn = document.getElementById('hamburger-button')
+//     const mobileMenu = document.getElementById('mobile-menu')
+
+//     const toggleMenu = () => {
+//         mobileMenu.classList.toggle('hidden')
+//         mobileMenu.classList.toggle('flex')
+//         hamburgerBtn.classList.toggle('toggle-btn')
+//     }
+
+//     hamburgerBtn.addEventListener('click', toggleMenu)
+//     mobileMenu.addEventListener('click', toggleMenu)
+// }
+
+// document.addEventListener('DOMContentLoaded', initApp)
