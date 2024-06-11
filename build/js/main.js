541ba6e1f5fed1747ebfdb5d9a3462566810697e
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-06-11 10:26:03 +0200

    done with part 4 of the project

--- build/js/main.js
+++ build/js/main.js
@@ -8,6 +8,7 @@ const initApp = () => {
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden');
         mobileMenu.classList.toggle('flex');
+        hamburgerBtn.classList.toggle('toggle-btn');
     }
 
     hamburgerBtn.addEventListener( 'click', toggleMenu);
@@ -20,18 +21,3 @@ document.addEventListener('DOMContentLoaded', initApp);
 
 
 
-// const initApp = () => {
-//     const hamburgerBtn = document.getElementById('hamburger-button')
-//     const mobileMenu = document.getElementById('mobile-menu')
-
-//     const toggleMenu = () => {
-//         mobileMenu.classList.toggle('hidden')
-//         mobileMenu.classList.toggle('flex')
-//         hamburgerBtn.classList.toggle('toggle-btn')
-//     }
-
-//     hamburgerBtn.addEventListener('click', toggleMenu)
-//     mobileMenu.addEventListener('click', toggleMenu)
-// }
-
-// document.addEventListener('DOMContentLoaded', initApp)
